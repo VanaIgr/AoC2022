fdc5da148e27588a07629f292a9bf3f4e18a6130
akkone609 <vanaigranov@gmail.com>
2023-06-03 15:01:51 -0500

    day 4

---
+++ Day4/part1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+
+int readNumber(std::istream &stream) {
+    int number = 0;
+    for(int input; (input = stream.get()) && input >= '0' && input <= '9';) {
+        number = number*10 + (input-'0');
+    }
+    return number;
+}
+
+int main() {
+	auto stream = std::ifstream{"part1.txt"};
+    	
+    int subsumed = 0;
+	while(stream.peek() != std::char_traits<char>::eof()) {
+        int n1 = readNumber(stream);
+        int n2 = readNumber(stream);
+        int n3 = readNumber(stream);
+        int n4 = readNumber(stream);
+
+        if((n1 <= n3 && n4 <= n2)
+            | (n3 <= n1 && n2 <= n4)
+        ) subsumed++;
+    }
+
+    std::cout << subsumed << '\n';
+
+    return 0;
+}
